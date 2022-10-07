05cf9fcc0f9de747991b4b0574211f94f2f4027d
Kulshrch <charukulshrestha1998@gmail.com>
2022-10-08 01:09:18 +0530

    LnBCal

---
+++ LnBcal.py
@@ -0,0 +1,9 @@
+while(1):
+
+    a=int(input("Enter a number:"))
+    b=int(input("Enter 2nd number:"))
+    print("Product=",a*b)
+    if(a*b>500):
+        print("Sum:",a+b)
+    else:    
+        print("Hello LNB code is running fine")
